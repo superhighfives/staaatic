8e1f4228329e150b420342bfa801b0b9afefcfcc
Charlie Gleason <cgleason@heroku.com>
2017-07-28 18:25:30 +0100

    Basic server

---
+++ app.js
@@ -0,0 +1,14 @@
+var connect = require('connect')
+var serveStatic = require('serve-static')
+
+var port = process.env.PORT || 3000
+
+var app = connect()
+
+app.use(serveStatic('public/', {
+  'index': ['index.html']
+}))
+
+app.listen(port)
+
+console.log('Server starting on http://0.0.0.0:%s', port)
